5bbe6d9cdd0429cf52c02c8cca40bf009d1ca713
David Lee <ddlee.907@gmail.com>
2016-02-16 12:20:35 -0500

    update

--- app.js
+++ app.js
@@ -12,23 +12,21 @@ app.use(express.static('./client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res, next){
-  res.sendFile(__dirname + '/client/views/index.html');
-});
-
 // Custom Middleware for Accessing APIs
 app.use(function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "https://s3.amazonaws.com");
+  res.header("Access-Control-Allow-Origin", "*");
   next();
+});
+
+app.get('/', function(req, res, next){
+  res.sendFile(__dirname + '/client/views/index.html');
 });
 
 app.get('/key', function(req, res){
 	var dUrl = "https://api.dribbble.com/v1/shots?&timeframe=week";
 	var page = page || 1;
     var limit = limit || 21;
-	request(dUrl + process.env.D_API_TOKEN + '&page=' + page + '&per_page=' + limit, function(err, response, body){
-	console.log(response)
-	res.json(JSON.parse(body))
+	request(dUrl + process.env.D_API_TOKEN + '&page=' + page + '&per_page=' + limit, function(err, response, body){	res.json(JSON.parse(body))
 	});
 });
 
