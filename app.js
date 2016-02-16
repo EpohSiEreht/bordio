0a371939acfeffe6fb456510be1aeb1818f26c88
David Lee <ddlee.907@gmail.com>
2016-02-16 11:18:53 -0500

    figure out request for api keys

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-require('dotenv').load();
+var env = require('dotenv').load();
+var request = require('request');
+var key = process.env.D_API_TOKEN;
+
+
 
 app.use(express.static('./client'));
+
 
 
 app.use(bodyParser.json());
@@ -11,6 +16,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res, next){
   res.sendFile(__dirname + '/client/views/index.html');
+});
+
+app.get('/key', function(req, res){
+	var dUrl = "https://api.dribbble.com/v1/shots?&timeframe=week"
+	var page = page || 1;
+    var limit = limit || 21;
+	var dCallback = '&callback=JSON_CALLBACK'
+	request(dUrl + process.env.D_API_TOKEN + '&page=' + page + '&per_page=' + limit + dCallback, function(err, response, body){
+	console.log(response)
+	res.json(body)
+	});
 });
 
 var port = 8080;
--- client/services/bordioApi.js
+++ client/services/bordioApi.js
@@ -1,4 +1,5 @@
 var api = angular.module('bordioApiFactory', []);
+
 api.factory('bordioApi', ['$http', function($http){
 
   var behanceUrl = 'http://www.behance.net/v2/projects?';
@@ -7,23 +8,26 @@ api.factory('bordioApi', ['$http', function($http){
   var dribbbleUrl = 'https://api.dribbble.com/v1/shots?';
   var dTimeWeek = '&timeframe=week';
   var dListGif = '&list=animated'
-  var dToken = D_API_TOKEN;
   var dPage = dPage || 1;
   var dPerPage = dPerPage || 21;
   var dCallback = '&callback=JSON_CALLBACK'
   var bordioInterface = {};
 
-  bordioInterface.getDribbbleShots = function(page, limit){
-    var page = page || 1;
-    var limit = limit || 21;
-    return $http.jsonp(dribbbleUrl + dTimeWeek + dToken + '&page=' + page + '&per_page=' + limit + dCallback );
-  }
+  $http.get('/key', function(err, done){
+    console.log(done);
+  });
 
-  bordioInterface.getDribbbleGifs = function(page, limit){
-    var page = page || 1;
-    var limit = limit || 21;
-    return $http.jsonp(dribbbleUrl + dTimeWeek + dListGif + dToken + "&page=" + page + "&per_page=" + limit + dCallback);
-  }
+  // bordioInterface.getDribbbleShots = function(page, limit){
+  //   var page = page || 1;
+  //   var limit = limit || 21;
+  //   return $http.jsonp(dribbbleUrl + dTimeWeek + API_KEY1 + '&page=' + page + '&per_page=' + limit + dCallback );
+  // }
+
+  // bordioInterface.getDribbbleGifs = function(page, limit){
+  //   var page = page || 1;
+  //   var limit = limit || 21;
+  //   return $http.jsonp(dribbbleUrl + dTimeWeek + dListGif + API_KEY1 + "&page=" + page + "&per_page=" + limit + dCallback);
+  // }
 
 
   return bordioInterface;
